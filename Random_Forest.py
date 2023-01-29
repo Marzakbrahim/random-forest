5ce1dbc03149663417e129d1ce1359a9ce0097ae
Marzakbrahim <marzakbrahim15@gmail.com>
2023-01-29 22:24:26 +0100

    premier code

--- Random_Forest.py
+++ Random_Forest.py
@@ -91,7 +91,7 @@ Cible_classe=donneesFinal[:,-1]
 ##### creation de modèle de préduction :#
 #########################################
 x_train, x_test, y_train, y_test  = train_test_split(features_classes,Cible_classe,test_size=0.25,random_state=42)
-
+#
 modele_rf = RandomForestClassifier(n_estimators=100,criterion='gini',max_depth=None,min_samples_split=2,min_samples_leaf=1,min_weight_fraction_leaf=0.0,max_features='auto',max_leaf_nodes=None,min_impurity_decrease=0.0,bootstrap=True,oob_score=False,n_jobs=None,random_state=None,verbose=0,warm_start=False,class_weight=None,ccp_alpha=0.0,max_samples=None,)
 modele_rf.fit(x_train, y_train)
 
